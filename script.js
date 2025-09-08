272663023052829176d732ab89884fe3e77b3bac
27janani <sce22is056@sairamtap.edu.in>
2025-09-08 22:11:43 +0530

    condition

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 function calculateBMI() {
-    const weightInput = prompt('Enter your weight in kilograms:');
-    const heightInput = prompt('Enter your height in meters:');
+  const weightInput = prompt("Enter your weight in kilograms:");
+  const heightInput = prompt("Enter your height in meters:");
 
+  const weight = parseFloat(weightInput);
+  const heightCm = parseFloat(heightInput);
 
-
-const weight = parseFloat(weightInput);
-    const heightCm = parseFloat(heightInput); 
-
-
-    let height = heightCm/100;
-
-    let bmi = weight/(height*height);
-
-    document.getElementById('result').innerText = `Your BMI is ${bmi.toFixed(2)}`;
-
+  document.getElementById("result").innerText = `Your BMI is ${bmi.toFixed(2)}`;
 }
